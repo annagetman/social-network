9872ae5f448d5154a5348197fb510043b55ebaa5
annagetman <annaporik12@gmail.com>
2020-08-03 11:28:50 +0300

    add new component

--- social_network/src/App.js
+++ social_network/src/App.js
@@ -1,28 +1,21 @@
 import React from 'react';
-import logo from './logo.png'
-import fon from './fon.jpg'
-import seagull from './seagull.jpg'
+import Header from "./components/Header";
 import './App.css';
+import Nav from "./components/Navbar";
+import Profile from "./components/Profile";
 
 function App() {
     return (
         <div className="app-wrapper">
-            <header className='header'>
-                <img src={logo}/>
-            </header>
 
-            <nav className='nav'>
-                <div><a>Profile</a></div>
-                <div><a>Messages</a></div>
-                <div><a>News</a></div>
-                <div><a>Music</a></div>
-                <div><a>Settings</a></div>
-            </nav>
-            <div className='content'>
-                <img src={fon}/>
 
-          <div> <img src={seagull}/>ava + description</div>
-        </div>
+            <Header/>
+
+            <Nav/>
+
+            <Profile/>
+
+
         </div>
     );
 }
