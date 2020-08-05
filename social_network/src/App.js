7ec2dc36ae8f56aec2edd72cf623bb05566a0266
annagetman <annaporik12@gmail.com>
2020-08-05 14:00:33 +0300

    created component Dialog

--- social_network/src/App.js
+++ social_network/src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 import './App.css';
-import Nav from "./components/Navbar";
-import Profile from "./components/Profile";
+import Nav from "./components/Navbar/Navbar";
+import Dialogs from "./components/Dialogs/Dialogs"
 
 function App() {
     return (
         <div className="app-wrapper">
 
-
             <Header/>
 
             <Nav/>
+            <div className="app-wrapper-content">
+                <Dialogs/>
+            </div>
+            {/*<Profile/>*/}
 
-            <Profile/>
 
 
         </div>
