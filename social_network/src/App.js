1732bae2a2099e8d540428cbc2f8bea23708096d
annagetman <annaporik12@gmail.com>
2020-08-05 15:35:50 +0300

    created component News, Music, Setting(use Router)

--- social_network/src/App.js
+++ social_network/src/App.js
@@ -2,23 +2,28 @@ import React from 'react';
 import Header from "./components/Header/Header";
 import './App.css';
 import Nav from "./components/Navbar/Navbar";
-import Dialogs from "./components/Dialogs/Dialogs"
+import Dialogs from "./components/Dialogs/Dialogs";
+import Profile from "./components/Profile/Profile";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Setting from "./components/Setting/Setting";
+import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
     return (
+        <BrowserRouter>
         <div className="app-wrapper">
-
             <Header/>
-
             <Nav/>
             <div className="app-wrapper-content">
-                <Dialogs/>
+                <Route path='/profile' component={Profile} />
+                <Route path='/dialogs' component={Dialogs} />
+                <Route path='/news' component={News} />
+                <Route path='/music' component={Music} />
+                <Route path='/setting' component={Setting} />
             </div>
-            {/*<Profile/>*/}
-
-
-
         </div>
+        </BrowserRouter>
     );
 }
 
