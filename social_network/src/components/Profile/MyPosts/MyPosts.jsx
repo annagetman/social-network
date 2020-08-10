5f7c7e30d235c7bf1d51d0d5eb9b6a53586d477b
annagetman <annaporik12@gmail.com>
2020-08-10 13:44:58 +0300

    update component dialog(use method map)

--- social_network/src/components/Profile/MyPosts/MyPosts.jsx
+++ social_network/src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,10 +5,11 @@ import Post from "./Post/Post";
 
 function MyPosts() {
 
-    let postData = [
+    let posts = [
         {id: 1, message: "hi, how are you?", likesCount: 12},
         {id: 2, message: "Its my first post", likesCount: 66},
     ]
+    let postsElements = posts.map((p) => <Post message={p.message} likesCount={p.likesCount} src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQFp3uCMFT-t3WKaunzc5iJMMNDiwDmz4q4CA&usqp=CAU'/>)
     return (
         <div className={classes.postsBlock}><h1>My Posts</h1>
             <div>
@@ -16,9 +17,9 @@ function MyPosts() {
             <div><button>Add post</button></div>
 
             <div className={classes.posts}>
-
-            <Post message={postData[0].message} likesCount={postData[0].likesCount} src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQFp3uCMFT-t3WKaunzc5iJMMNDiwDmz4q4CA&usqp=CAU'/>
-            <Post message={postData[1].message} likesCount={postData[1].likesCount} src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRc96kcLicYy25CFi7P_ocMargwSC_vjRxIMg&usqp=CAU'/>
+                {postsElements}
+            {/*<Post message={posts[0].message} likesCount={posts[0].likesCount} src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQFp3uCMFT-t3WKaunzc5iJMMNDiwDmz4q4CA&usqp=CAU'/>*/}
+            {/*<Post message={posts[1].message} likesCount={posts[1].likesCount} src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRc96kcLicYy25CFi7P_ocMargwSC_vjRxIMg&usqp=CAU'/>*/}
 
 
 
