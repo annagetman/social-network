b1c00d09814c65627aeff56422dbb47a504d48a1
annagetman <annaporik12@gmail.com>
2020-08-13 14:31:26 +0300

    add state(new arr photo)

--- social_network/src/App.js
+++ social_network/src/App.js
@@ -7,20 +7,23 @@ import Profile from "./components/Profile/Profile";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Setting from "./components/Setting/Setting";
+import Friends from "./components/Friends/Friends";
 import {BrowserRouter, Route} from "react-router-dom";
 
-function App() {
+function App(props) {
+
     return (
         <BrowserRouter>
         <div className="app-wrapper">
             <Header/>
             <Nav/>
             <div className="app-wrapper-content">
-                <Route exact path='/profile' render={ () => <Profile/>} />
-                <Route path='/dialogs' render={ () => <Message/>} />
-                <Route path='/news' render={ () => <Profile/>} />
+                <Route exact path='/profile' render={ () => <Profile state={props.state.profilePage}/>} />
+                <Route path='/dialogs' render={ () => <Message state={props.state.messagesPage}/>} />
+                <Route path='/news' render={ () =>  <News />} />
                 <Route path='/music' render={ () => <Music/>} />
                 <Route path='/setting' render={ () => <Setting/>} />
+                <Route path='/friends' render={ () => <Friends/>} />
             </div>
         </div>
         </BrowserRouter>
