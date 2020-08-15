16492da7d85453805bae4464fc603083d1bb15c0
annagetman <annaporik12@gmail.com>
2020-08-15 21:34:01 +0300

    onClick-error

--- social_network/src/components/Profile/MyPosts/MyPosts.jsx
+++ social_network/src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import classes from './MyPosts.module.css';
 import Post from "./Post/Post";
+import {addPost} from "../../redux/state";
 
 
-function MyPosts() {
+addPost('samurayJS');
 
-    let posts = [
-        {id: 1, message: "hi, how are you?", likesCount: 12},
-        {id: 2, message: "Its my first post", likesCount: 66},
-    ]
-    let postsElements = posts.map((p) => <Post message={p.message} likesCount={p.likesCount} src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQFp3uCMFT-t3WKaunzc5iJMMNDiwDmz4q4CA&usqp=CAU'/>)
+
+function MyPosts(props) {
+    let postsElements = props.posts.map( p => <Post message={p.message} likesCount={p.likesCount} src={p.src} />)
+
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+       props.addPost(text)
+    }
+
     return (
         <div className={classes.postsBlock}><h1>My Posts</h1>
             <div>
-                <textarea></textarea></div>
-            <div><button>Add post</button></div>
+                <textarea ref={newPostElement}></textarea></div>
+            <div><button onClick={addPost}>Add post</button></div>
 
             <div className={classes.posts}>
                 {postsElements}
-            {/*<Post message={posts[0].message} likesCount={posts[0].likesCount} src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQFp3uCMFT-t3WKaunzc5iJMMNDiwDmz4q4CA&usqp=CAU'/>*/}
-            {/*<Post message={posts[1].message} likesCount={posts[1].likesCount} src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRc96kcLicYy25CFi7P_ocMargwSC_vjRxIMg&usqp=CAU'/>*/}
-
-
-
             </div>
         </div>
     )
